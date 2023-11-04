cf7a15fc0fb18332f046e5c2a32900f52ef3d374
kanaru-ssk <2019january.0131@gmail.com>
2023-11-05 01:25:17 +0900

    mongodbから取得時に日付ソートするよう修正

--- src/lib/server/fetchAcwiDataFromMongo.ts
+++ src/lib/server/fetchAcwiDataFromMongo.ts
@@ -1,4 +1,5 @@
 import { database } from '$lib/server/mongodb';
 import type { AcwiData } from '$lib/type/AcwiData';
 
-export const fetchAcwiDataFromMongo = () => database.collection<AcwiData>('acwi').find().toArray();
+export const fetchAcwiDataFromMongo = () =>
+	database.collection<AcwiData>('acwi').find().sort({ date: 1 }).toArray();
