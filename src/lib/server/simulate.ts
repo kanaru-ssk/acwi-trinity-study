9d4e4cdc63d5d74da0a12900b5b4ff98723526c0
kanaru-ssk <2019january.0131@gmail.com>
2023-11-04 19:39:03 +0900

    add simulate.ts

---
+++ src/lib/server/simulate.ts
@@ -0,0 +1,5 @@
+import type { AcwiData } from '$lib/type/AcwiData';
+
+export const simulate = async (data: AcwiData[]) => {
+	return data;
+};
--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,12 +1,25 @@
 import { fetchAcwiData } from '$lib/server/fetchAcwiData';
+import { simulate } from '$lib/server/simulate';
 
 export const prerender = true;
 
 export const load = async () => {
 	const acwiData = await fetchAcwiData();
 
+	// TODO: fetchOriginalAcwiDataで最新データ取得
+	// TODO: 最新データがある場合は為替データ取得
+	// TODO: 最新データをMongoDBにinsert
+
+	const simulation = await simulate(acwiData);
+	console.log(simulation);
+
 	const acwiDataStr = acwiData.map((v) => {
-		return [v.date.toLocaleString(), String(v.price_usd), String(v.price_jpy), String(v.jpy_usd)];
+		return [
+			v.date.toLocaleDateString(),
+			String(v.price_usd),
+			String(v.price_jpy),
+			String(v.jpy_usd)
+		];
 	});
 
 	return { acwiData: acwiDataStr };
