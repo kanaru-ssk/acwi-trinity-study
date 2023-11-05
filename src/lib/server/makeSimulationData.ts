d274f7b903fc843b9e4c62ee9dd0b92bbdb3129c
kanaru-ssk <2019january.0131@gmail.com>
2023-11-06 01:49:47 +0900

    makeSimulationDataにコメント追加

--- src/lib/server/makeSimulationData.ts
+++ src/lib/server/makeSimulationData.ts
@@ -22,26 +22,39 @@ export const makeSimulationData = async (acwiData: AcwiData[]) => {
 };
 
 // 取崩しシミュレーション
+// チャートデータ、取崩し期間、取崩し率、試行回数を受け取って成功率(0~1)を返す
 const simulate = (
 	acwiData: AcwiData[],
 	payoutPeriod: number,
 	withdrawalRate: number,
 	numOfSimulation: number
 ) => {
-	let countFailure = 0;
+	let countFailure = 0; // 失敗回数
 	for (let i = 0; i < numOfSimulation; i++) {
-		let amountRemaining = acwiData[i].price_jpy;
+		let amountRemaining = acwiData[i].price_jpy; // 資産残高
+
+		// 毎年の取崩し額: 初年度の資産額に取崩し率をかけた額
 		const withdrawals = amountRemaining * withdrawalRate * 0.01;
 		for (let pi = 0; pi < payoutPeriod; pi++) {
-			if (pi !== 0)
-				amountRemaining *= acwiData[i + 12 * pi].price_jpy / acwiData[i + 12 * (pi - 1)].price_jpy;
+			if (pi !== 0) {
+				const prePrice = acwiData[i + 12 * (pi - 1)].price_jpy; // 1年前の基準価格
+				const price = acwiData[i + 12 * pi].price_jpy; // 当月の基準価格
+				const PercentageChange = price / prePrice; // 騰落率
 
+				// 資産残高に騰落率を掛ける
+				amountRemaining *= PercentageChange;
+			}
+
+			// 資産残高から取崩し額を引く
 			amountRemaining -= withdrawals;
-			if (amountRemaining < 0) {
+
+			// 資産残高が0になったら失敗
+			if (amountRemaining <= 0) {
 				countFailure++;
 				break;
 			}
 		}
 	}
+
 	return (numOfSimulation - countFailure) / numOfSimulation;
 };
