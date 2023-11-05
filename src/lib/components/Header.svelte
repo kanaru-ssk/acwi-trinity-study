70cf6d82ff4df847aaed5057e95cf85980332aa6
kanaru-ssk <2019january.0131@gmail.com>
2023-11-06 00:22:21 +0900

    Headerコンポーネント追加

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,25 @@
+<header>
+	<img src="/logo.svg" alt="logo" />
+</header>
+
+<style>
+	header {
+		position: sticky;
+		top: 0;
+		padding: 12px 16px;
+	}
+
+	img {
+		height: 32px;
+	}
+
+	@media (min-width: 640px) {
+		header {
+			padding: 24px 48px;
+		}
+
+		img {
+			height: 48px;
+		}
+	}
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
+	import Header from '$lib/components/Header.svelte';
 	import '$lib/styles/global.css';
 </script>
+
+<Header />
 
 <slot />
