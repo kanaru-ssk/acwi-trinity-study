ca70a5e784f41167b1e7fdcbe7d25c3e998ba1c4
kanaru-ssk <2019january.0131@gmail.com>
2023-11-05 13:13:18 +0900

    msciから最新データ取得機能追加

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,4 +1,5 @@
 import { fetchAcwiDataFromMongo } from '$lib/server/fetchAcwiDataFromMongo';
+import { fetchAcwiDataFromMsci } from '$lib/server/fetchAcwiDataFromMsci';
 import { makeSimulationData, payoutPeriods, withdrawalRates } from '$lib/server/makeSimulationData';
 
 export const prerender = true;
@@ -6,7 +7,10 @@ export const prerender = true;
 export const load = async () => {
 	const acwiData = await fetchAcwiDataFromMongo();
 
-	// TODO: fetchAcwiDataFromOriginで最新データ取得
+	const lastDataDate = acwiData[acwiData.length - 1].date;
+
+	const originData = await fetchAcwiDataFromMsci(lastDataDate);
+	console.log(originData);
 	// TODO: 最新データがある場合は為替データ取得
 	// TODO: 最新データをMongoDBにinsert
 
