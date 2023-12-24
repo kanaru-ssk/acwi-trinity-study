29f0354ad2f708f63803d65790fa28cc1111c721
kanaru-ssk <2019january.0131@gmail.com>
2023-12-24 10:00:00 +0900

    fix numSimulation

--- src/lib/server/make-simulation.ts
+++ src/lib/server/make-simulation.ts
@@ -8,7 +8,7 @@ export const withdrawalRates = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 export const makeSimulation = async (acwiData: AcwiData[]) => {
   // データの年数からシミュレーション可能な取り崩し期間を算出
   // 取り崩し期間は15年以上で5年区切り
-  const numYearOfData = Math.floor(432 / 12);
+  const numYearOfData = Math.floor(acwiData.length / 12);
   const numSimulate = Math.floor(numYearOfData / 5) - 2;
   const payoutPeriods = Array.from(
     { length: numSimulate },
